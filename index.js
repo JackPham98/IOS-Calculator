7d5b6fe5226df8e0719da598a1da370364021cac
JackPham98 <khoadangpham98@gmail.com>
2022-11-02 15:39:31 +0700

    I do my way

--- index.js
+++ index.js
@@ -1,71 +1,73 @@
+//Manipulate all the elements and declare variables for them in this JS file.
+const signs = document.querySelectorAll('.operators');
+const clear = document.querySelector('.ac');
+const np = document.querySelector('.negative');
+const percent = document.querySelector('.percent');
+const equal = document.querySelector('.equal-sign');
+const comma = document.querySelector('.comma');
 const numbers = document.querySelectorAll('.numbers');
-const result = document.querySelector('.results span');
-const signs = document.querySelectorAll('operators');
-const equal = document.querySelector('equal-sign');
-const clear = document.querySelector('clear');
-const negative = document.querySelector('negative');
-const percent = document.querySelector('percent');
+const result = document.querySelector('.results')
 
-
-
-
-
-
-let firstValue = "";
-let isFirstValuePresent = false;
-let secondValue = "";
-let isSecondValuePresent = false;
+//Initialize all necessary variables for this calculator.
+let firstVa = 0;
+let secondVa = "";
+let currentVa = 0;
 let sign = "";
-let resultValue = 0;
+let nSign = "+1";
+let percentPresent = "1";
 
-for (let i = 0; i < numbers.length; i++) {
+result.innerText = firstVa;
+
+for(i=1; i < numbers.length; i++){
     numbers[i].addEventListener('click', (e) => {
-        let atr = e.target.getAttribute('value');
-        if (isFirstValuePresent === false) {
-            getFirstValue(atr);
-        }
-
-        if (isSecondValuePresent === false) {
-            getSecondValue(atr);
+        if(firstVa === 0 && sign === ""){
+            firstVa = "";
+            firstVa = (firstVa + e.target.getAttribute('value')) * nSign * percentPresent;
+            // currentVa = firstVa;
+            result.innerText = firstVa;
+        } else if(typeof firstVa === String || typeof firstVa === Number && sign != ""){
+            // secondVa = "";
+            secondVa = (secondVa + e.target.getAttribute('value')) * nSign * percentPresent;
+            // currentVa = secondVa;
+            result.innerText = secondVa;
         }
     })
 }
 
-function getFirstValue(el) {
-    result.innerText = "";
-    firstValue += el;
-    result.innerText = firstValue;
-    firstValue = +firstValue;
-}
-
-function getSecondValue(el) {
-    if (firstValue != "" && sign != "") {
-        secondValue += el;
-        result.innerText = secondValue;
-        secondValue = +secondValue;
-    }
-}
-
-function getSign() {
-    for (let i = 0; i < signs.length; i++) {
+function getSign(){
+    for(i=0; i < signs.length; i++){
         signs[i].addEventListener('click', (e) => {
             sign = e.target.getAttribute('value');
-            isFirstValuePresent = true; 
         })
     }
 }
-getSign();
+getSign;
+
+
 
 equal.addEventListener('click', () => {
     result.innerText = "";
+    firstVa = parseInt(firstVa);
+    secondVa = parseInt(secondVa);
+    console.log(`firstis ${firstVa}`)
+    console.log(typeof firstVa)  
+    console.log(`secondis ${secondVa}`)
+    console.log(typeof firstVa)  
+    console.log(sign);
+    console.log(typeof sign);
+    // calculateResult(firstVa,secondVa);
     if(sign === "+") {
-        resultValue = firstValue + secondValue;
+        currentVa = firstVa + secondVa;
     } else if (sign === "-") {
-        resultValue = firstValue - secondValue;
-    } else if(sign === "/") {
-        resultValue = firstValue / secondValue;
-    } else if(sign === "x") {
-        resultValue = firstValue * secondValue;
+        currentVa = firstVa - secondVa;
+    } else if (sign === "x") {
+        currentVa = firstVa * secondVa;
+    } else if (sign === "/") {
+        currentVa = firstVa / secondVa;
     }
-    result.innerText = resultValue;
+
+
+    console.log(`result ${currentVa}`)
+    firstVa = currentVa;
+    result.innerText = firstVa;
 })
