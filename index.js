2dba593263285b621337cce97cb03f77287694e3
JackPham98 <khoadangpham98@gmail.com>
2022-11-03 12:09:48 +0700

    done

--- index.js
+++ index.js
@@ -4,70 +4,108 @@ const clear = document.querySelector('.ac');
 const np = document.querySelector('.negative');
 const percent = document.querySelector('.percent');
 const equal = document.querySelector('.equal-sign');
-const comma = document.querySelector('.comma');
 const numbers = document.querySelectorAll('.numbers');
 const result = document.querySelector('.results')
 
 //Initialize all necessary variables for this calculator.
-let firstVa = 0;
+let firstVa = "";
 let secondVa = "";
 let currentVa = 0;
 let sign = "";
-let nSign = "+1";
-let percentPresent = "1";
+let nSign = "-1";
 
-result.innerText = firstVa;
+result.innerText = 0;
+
+clear.addEventListener('click', ()=> {
+//Set default values:
+    firstVa = "";
+    secondVa = "";
+    currentVa = 0;
+    sign = "";
+    nSign = "-1";
+    result.innerText = 0;
+}
+)
 
 for(i=1; i < numbers.length; i++){
     numbers[i].addEventListener('click', (e) => {
-        if(firstVa === 0 && sign === ""){
-            firstVa = "";
-            firstVa = (firstVa + e.target.getAttribute('value')) * nSign * percentPresent;
-            // currentVa = firstVa;
+        if((typeof firstVa === "string" || typeof firstVa === "number") && sign === ""){
+            firstVa = (firstVa + e.target.getAttribute('value'));
             result.innerText = firstVa;
-        } else if(typeof firstVa === String || typeof firstVa === Number && sign != ""){
-            // secondVa = "";
-            secondVa = (secondVa + e.target.getAttribute('value')) * nSign * percentPresent;
-            // currentVa = secondVa;
+            firstVa = firstVa.toString();
+        } else if((typeof firstVa === "string" || typeof firstVa === "number") && sign != ""){
+            secondVa = (secondVa + e.target.getAttribute('value'));
             result.innerText = secondVa;
+            secondVa = secondVa.toString();
         }
     })
 }
+
+percent.addEventListener('click',() => {
+    if(sign === "") {
+        firstVa = firstVa * "0.01";
+        result.innerText = firstVa;
+        
+    } else if (sign != "") {
+        secondVa = secondVa * "0.01";
+        result.innerText = secondVa;
+    }
+})
+
+np.addEventListener('click', () => {
+    if(sign === "") {
+        firstVa = firstVa * nSign;
+        result.innerText = firstVa;
+        firstVa = firstVa.toString();
+    } else if (sign != "") {
+        secondVa = secondVa * nSign;
+        result.innerText = secondVa;
+        secondVa = secondVa.toString();
+    } 
+})
 
 function getSign(){
     for(i=0; i < signs.length; i++){
         signs[i].addEventListener('click', (e) => {
             sign = e.target.getAttribute('value');
+            if (firstVa != ""){
+                firstVa = firstVa.toString();;
+            } else if( firstVa === ""){
+                firstVa = 0;
+                result.innerText = firstVa;
+                firstVa = firstVa.toString();
+            }
         })
     }
 }
-getSign;
+getSign();
 
 
 
 equal.addEventListener('click', () => {
     result.innerText = "";
-    firstVa = parseInt(firstVa);
-    secondVa = parseInt(secondVa);
-    console.log(`firstis ${firstVa}`)
-    console.log(typeof firstVa)  
-    console.log(`secondis ${secondVa}`)
-    console.log(typeof firstVa)  
-    console.log(sign);
-    console.log(typeof sign);
-    // calculateResult(firstVa,secondVa);
-    if(sign === "+") {
+    console.log(`first value is ${firstVa}`)
+    console.log(`second value is ${secondVa}`)
+    firstVa = parseFloat(firstVa);
+    secondVa = parseFloat(secondVa);
+    console.log(typeof firstVa);
+    console.log(typeof secondVa);
+    if(sign === "+" && typeof firstVa === "number" && typeof secondVa === "number") {
         currentVa = firstVa + secondVa;
-    } else if (sign === "-") {
+    } else if (sign === "-" && typeof firstVa === "number" && typeof secondVa === "number") {
         currentVa = firstVa - secondVa;
-    } else if (sign === "x") {
+    } else if (sign === "x" && typeof firstVa === "number" && typeof secondVa === "number") {
         currentVa = firstVa * secondVa;
-    } else if (sign === "/") {
+    } else if (sign === "/" && typeof firstVa === "number" && typeof secondVa === "number") {
         currentVa = firstVa / secondVa;
-    }
-
-
-    console.log(`result ${currentVa}`)
+    } 
     firstVa = currentVa;
+    firstVa = firstVa.toString();
     result.innerText = firstVa;
+
+//Set deafault values:
+    secondVa = "";
+    currentVa = 0;
+    sign = "";
+    nSign = "-1";
 })
